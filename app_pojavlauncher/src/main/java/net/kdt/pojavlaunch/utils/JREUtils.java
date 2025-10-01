cac3c1199e003f8aae4f402b637aa52c1580ed69
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-10-01 12:50:05 +0800

    fix crash

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -221,6 +221,8 @@ public class JREUtils {
             }
             if(LOCAL_RENDERER.equals("opengles_mobileglues")){
                 envMap.put("MG_DIR_PATH", Tools.DIR_DATA + "/MobileGlues");
+                envMap.put("LIBGL_ES", "3");
+                envMap.put("POJAVEXEC_EGL","libmobileglues.so");
             }
         }
         if(LauncherPreferences.PREF_BIG_CORE_AFFINITY) envMap.put("POJAV_BIG_CORE_AFFINITY", "1");
